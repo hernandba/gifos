09cc2e8d2a1d302f3fe8bae94c7dc3a79d464211
Hernan Belalcazar <hernandba@gmail.com>
2021-01-30 23:16:33 -0500

    Arreglo en updateFaves para misGifosPage

--- scripts/favs.js
+++ scripts/favs.js
@@ -5,23 +5,24 @@ let favGifsEmpty = document.querySelector('#fav-gifs-empty'),
     favGifsMoreBtn = document.querySelector('#fav-gifs-more-btn'),
     trendingGifs = document.querySelector('#trending-gifs-container');
 
-function updateFavs(){
-    if (favGifs.length > 0) {
-        favGifsResults.innerHTML = "";
-        favGifsEmpty.classList.add('hide');
-        favGifsResults.classList.remove('hide');
-        createGifcards(favGifs, favGifsResults);
-        if (favGifs.length > 12) {
-            favGifsMoreBtn.classList.remove('hide');
+function updateFavs() {
+    if (favGifsResults != null) {
+        if (favGifs.length > 0) {
+            favGifsResults.innerHTML = "";
+            favGifsEmpty.classList.add('hide');
+            favGifsResults.classList.remove('hide');
+            createGifcards(favGifs, favGifsResults);
+            if (favGifs.length > 12) {
+                favGifsMoreBtn.classList.remove('hide');
+            } else {
+                favGifsMoreBtn.classList.add('hide');
+            }
         } else {
-            favGifsMoreBtn.classList.add('hide');
+            favGifsResults.innerHTML = "";
+            favGifsResults.classList.add('hide');
+            favGifsEmpty.classList.remove('hide');
         }
-    } else if(favGifsResults != null){
-        favGifsResults.innerHTML = "";
-        favGifsResults.classList.add('hide');
-        favGifsEmpty.classList.remove('hide');
     }
 }
 
 updateFavs();
-
