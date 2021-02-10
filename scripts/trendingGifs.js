573275e44464d7764d09b4a4ab732b53c21fd91c
Hernan Belalcazar <hernandba@gmail.com>
2021-02-10 15:47:34 -0500

    Slider con flechas para trendingGifs

--- scripts/trendingGifs.js
+++ scripts/trendingGifs.js
@@ -1,8 +1,32 @@
 /* -------------------------------------------------------------------------- */
 /*                                TRENDING GIFS                               */
 /* -------------------------------------------------------------------------- */
-let trendingGifsContainer = document.querySelector('#trending-gifs-container');
+let trendingGifsContainer = document.querySelector('#trending-gifs-container')
+    leftArrow = document.querySelector('.arrow-left'),
+    rightArrow = document.querySelector('.arrow-right'),
+    trendingGifsSlider = document.querySelector('#trending-gifs-slider');
+
+let scrollSize;
+trendingGifsSlider.scrollLeft = 0;
+console.log(trendingGifsSlider.style)
 
 getApiInfo(urlTrendingGifs).then(data => {
     createGifcards(data, trendingGifsContainer);
+
+    leftArrow.addEventListener('click', event => {
+        
+        scrollSize = trendingGifsSlider.scrollLeft - 1000;
+        if(scrollSize < 0){scrollSize = 0};
+        trendingGifsSlider.scroll(scrollSize, 0);
+        // console.log(`scrollSize = ${scrollSize}`)
+        event.stopPropagation();
+    })
+    
+    rightArrow.addEventListener('click', event => {
+        scrollSize = trendingGifsSlider.scrollLeft + 1000;
+        trendingGifsSlider.scroll(scrollSize, 0);
+        // console.log(`scrollSize = ${scrollSize}`)
+        event.stopPropagation();
+    })
+
 }).catch(console.error);
